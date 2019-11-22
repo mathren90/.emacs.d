f6044272b9a0ef4ec2b8fc60108522626230f740
Mathieu <m.renzo@uva.nl>
2019-11-22 10:52:56 -0500

    added minimal config

--- minimal.el
+++ minimal.el
@@ -4,6 +4,8 @@
 (setq inhibit-startup-message t) ;; hide the startup message
 (setq inhibit-startup-echo-area-message t)
 (setq initial-scratch-message nil)
+(setq frame-title-format '("" "%b @ Emacs " emacs-version))
+
 
 (global-linum-mode t) ;; enable line numbers globally
 
