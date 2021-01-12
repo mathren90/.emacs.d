393284dfbb06e975c3d55c49eb45fbd7a2cb315e
math <mathren90@gmail.com>
2021-01-11 19:20:09 -0500

    added package requirement

--- install_packages.el
+++ install_packages.el
@@ -20,6 +20,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see http://www.gnu.org/licenses/.
 
+(require 'package)
+
+;; define list of my packages
 (setq package-list '(which-key
 		     use-package
 		     swiper
