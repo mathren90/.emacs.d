ac2dea04e67791ae1bf48673b1b7440b05e22828
math <mathren90@gmail.com>
2021-01-16 11:15:27 -0500

    manage config with org mode
    literate config

--- init.el
+++ init.el
@@ -1,41 +1,18 @@
-; Author: Mathieu Renzo <mathren90@gmail.com>
-; Keywords: emacs configuration
-
-; Copyright (C) 2019-2021 Mathieu Renzo
-
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or (at
-; your option) any later version.
-;
-; This program is distributed in the hope that it will be useful, but
-; WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-; General Public License for more details.
-;
-; You should have received a copy of the GNU General Public License
-; along with this program.  If not, see http://www.gnu.org/licenses/.
-
-;; load the bare minimum
 (load "~/.emacs.d/minimal.el")
 
-;; when in GUI ask for confirmation when closing emacs
-(setq-default confirm-kill-emacs (quote y-or-n-p))
-
-;; avoid line numbers in some modes
-(dolist (mode '(term-mode-hook
-                shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; start in server-mode
 (server-start)
 
-(require 'package)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\C-r" 'recentf-open-files)
 
+(set-default 'truncate-lines t)
+
+(require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org"   . "https://orgmode.org/elpa/")
+                         ("elpa"  . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (require 'use-package)
@@ -44,45 +21,28 @@
 
 (use-package all-the-icons)
 
-;; Dired file browser
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
-;; use only one buffer for dired
 (use-package dired-single)
 
-;; improve mode-line at the bottom
 (use-package doom-modeline
   :ensure t
   :custom ((doom-modeline-height 10))
   :init (doom-modeline-mode 1))
 
-;; recent files
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 25)
-(global-set-key "\C-x\C-r" 'recentf-open-files)
-
-;; handling parenthesis
-(electric-pair-mode 1)
-(setq electric-pair-preserve-balance nil)
+;; these are configured in minimal.el
+;; (electric-pair-mode 1)
+;; (setq electric-pair-preserve-balance nil)
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;; auto-revert when a file changes
-; (global-auto-revert-mode t)
-
-;; Activate toggle-truncate-lines from start
-(set-default 'truncate-lines t)
-
-;; which key for suggestions on shortcuts
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1))
 
-;; lsp mode
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init
@@ -95,36 +55,27 @@
   :custom
   (lsp-ui-doc-position 'bottom))
 
-;; pretty symbols
-(global-prettify-symbols-mode 1)
-
-;; highlight line
-;; (global-hl-line-mode -1)
-
-;; LaTeX configuration
-
-;; spell checking
 (dolist (hook '(text-mode-hook LaTeX-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
 (setq flyspell-sort-corrections nil)
 (setq flyspell-issue-message-flag nil)
 
-;; reftex
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (add-hook 'LaTex-mode-hook 'flyspell-mode)
 (setq reftex-plug-into-AUCTeX t)
-(setq reftex-default-bibliography '("/home/math/Documents/Research/Biblio_papers/bibtex/master_bibtex.bib"))
-;; ;; prevent linebreaks in math mode
+(setq reftex-default-bibliography '("~/Documents/Research/Biblio_papers/bibtex/master_bibtex.bib"))
+
 (add-hook 'LaTeX-mode-hook
           (lambda ()
             (add-to-list 'fill-nobreak-predicate 'texmathp)))
 
 (use-package org
   :config
-  ;; unbind Shift+arrows from org mode, as I use these for navigating buffers
   (define-key org-mode-map (kbd "<S-left>") nil)
   (define-key org-mode-map (kbd "<S-right>") nil)
-  (setq org-ellipsis " ⤵ ")
+  (define-key org-mode-map (kbd "<S-down>") nil)
+  (define-key org-mode-map (kbd "<S-up>") nil)
+  (setq org-ellipsis " ▾ ")
   (setq org-startup-with-inline-images t)
   (setq org-image-actual-width 400)
   (setq org-hide-emphasis-markers t) ;; hide synthax markers
@@ -141,15 +92,6 @@
 	  ))
     )
 
-;; to paste inline images with org-download
-;; pasting from clipboard requires "wl-paste" available on apt
-;; and can be done with =M-x org-download-clipboard=
-(use-package org-download
-  :config
-  ;; create hidden folder to store image files
-  (setq-default org-download-image-dir ".org_notes_figures/")
-  )
-
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode)
@@ -165,55 +107,18 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
-;;;;; keybindings
-;; org-related
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cr" 'org-capture)
-(define-key global-map "\C-ctl" 'org-todo-list)
+(use-package org-download
+  :config
+  (setq-default org-download-image-dir ".org_notes_figures/")
+  )
 
-;;jump to last (but one) line asking for column
-;; to define macro with user interaction
-;; (defun my-macro-query (arg)
-;;   "Prompt for input using minibuffer during kbd macro execution.
-;;  With prefix argument, allows you to select what prompt string to use.
-;;  If the input is non-empty, it is inserted at point."
-;;   (interactive "P")
-;;   (let* ((query (lambda () (kbd-macro-query t)))
-;;          (prompt (if arg (read-from-minibuffer "PROMPT: ") "Input: "))
-;;          (input (unwind-protect
-;;                     (progn
-;;                       (add-hook 'minibuffer-setup-hook query)
-;;                       (read-from-minibuffer prompt))
-;;                   (remove-hook 'minibuffer-setup-hook query))))
-;;     (unless (string= "" input) (insert input))))
-
-;; (global-set-key "\C-xQ" 'my-macro-query)
-;; see http://www.emacswiki.org/emacs/KeyboardMacros#toc4 to have an idea of how I came up with this solution
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; first define a function used in the combination below
-(defun go-to-column (column)
-  (interactive "nColumn: ")
-  (move-to-column column t))
-(global-set-key (kbd "M-g TAB") 'go-to-column)
-
-(fset 'last-line-which-col
-      "\C-[>\C-[OA\C-a\C-[g\C-i\C-u\C-xq[OB")
-
-(put 'last-line-which-col 'kmacro t)
-
-(global-set-key (kbd "C-c C-l") 'last-line-which-col)
-
-;; python autocompletion
 (use-package elpy
   :ensure t
   :init
   (elpy-enable))
 
-; to format python code with black
 (load "~/.emacs.d/emacs_tools/blacken.el")
 
-;; Enable Flycheck
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
@@ -240,15 +145,6 @@
   :config
   (ivy-mode 1))
 
-(use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (counsel-mode 1))
-
 (use-package ivy-prescient
   :after counsel
   :custom
@@ -258,6 +154,14 @@
   (prescient-persist-mode 1)
   (ivy-prescient-mode 1))
 
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (counsel-mode 1))
 
 (use-package company
   :after lsp-mode
@@ -276,3 +180,20 @@
 (use-package tramp
   :config
   (setq tramp-default-method "ssh"))
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cr" 'org-capture)
+(define-key global-map "\C-ctl" 'org-todo-list)
+
+(defun go-to-column (column)
+  (interactive "nColumn: ")
+  (move-to-column column t))
+(global-set-key (kbd "M-g TAB") 'go-to-column)
+
+(fset 'last-line-which-col
+      "\C-[>\C-[OA\C-a\C-[g\C-i\C-u\C-xq[OB")
+
+(put 'last-line-which-col 'kmacro t)
+
+(global-set-key (kbd "C-c C-l") 'last-line-which-col)
--- minimal.el
+++ minimal.el
@@ -1,54 +1,30 @@
-;; author: Mathieu Renzo
-
-;; Author: Mathieu Renzo <mathren90@gmail.com>
-;; Keywords: files
-
-;; Comment: minimal configuration for quickly looking up things
-
-;; Copyright (C) 2019-2021 Mathieu Renzo
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see http://www.gnu.org/licenses/.
-
-;; startup
 (setq inhibit-startup-message t)
 (setq inhibit-startup-echo-area-message t)
 (setq initial-scratch-message nil)
-;; UI
+
 (setq frame-title-format '("Emacs %@ %*" (:eval (if (buffer-name)(abbreviate-file-name (buffer-name)) "%b %*"))))
 (setq ring-bell-function 'ignore)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode 0)
 (set-fringe-mode 0)
-
-;; line and column number
 (column-number-mode)
 (global-display-line-numbers-mode t)
-;; handling parenthesis
+(load-theme 'wombat t)
+
+(add-hook 'before-save-hook '(lambda()
+                              (when (not (or (derived-mode-p 'markdown-mode)))
+                                (delete-trailing-whitespace))))
+
 (electric-pair-mode 1)
 (setq electric-pair-preserve-balance nil)
 
-;; theme
-(load-theme 'wombat t) ;; use whiteboard or default for light theme
-;; open .bash_* in sh-script-mode
 (add-to-list 'auto-mode-alist '("/\.bash[^/]*$" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("/dot-bash[^/]*$" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("/\.zsh[^/]*$" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("/dot-zsh[^/]*$" . shell-script-mode))
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
-;; zoom-in and out
 (defun zoom-in ()
   (interactive)
   (let ((x (+ (face-attribute 'default :height)
@@ -60,51 +36,27 @@
   (let ((x (- (face-attribute 'default :height)
               10)))
     (set-face-attribute 'default nil :height x)))
-
-;; define global zoom in/out
-;; N.B. per buffer zoom in/out can be achieved with C-x C-+ and C-x C--
 (define-key global-map (kbd "C-+") 'zoom-in)
 (define-key global-map (kbd "C--") 'zoom-out)
 
-;; use shift + arrows to change buffer
 (when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+   (windmove-default-keybindings))
 
-;; C-pgUp/pgDown to browse buffers
-(global-set-key (kbd "C-<prior>") 'previous-buffer)
-(global-set-key (kbd "C-<next>") 'next-buffer)
+ (global-set-key (kbd "C-<prior>") 'previous-buffer)
+ (global-set-key (kbd "C-<next>") 'next-buffer)
 
-;; allow more garbage before collection
-(setq gc-cons-threshold 25000000) ;; 25Mb
-;; no backup files
-; (setq make-backup-files nil)
-;; and move autosaves to /tmp
+(global-set-key (kbd "\C-x k") 'kill-this-buffer)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(setq gc-cons-threshold 25000000)
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; ;; ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; autoindent with return key
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-;; delete trailing white spaces except for markdown
-(add-hook 'before-save-hook '(lambda()
-                              (when (not (or (derived-mode-p 'markdown-mode)))
-                                (delete-trailing-whitespace))))
-
-;; don't ask which buffer when killing
-(global-set-key (kbd "\C-x k") 'kill-this-buffer)
-
-;; open dot-bash* .bash* in sh-script-mode
-(add-to-list 'auto-mode-alist '("/\.bash[^/]*$" . shell-script-mode))
-(add-to-list 'auto-mode-alist '("/dot-bash[^/]*$" . shell-script-mode))
-(add-to-list 'auto-mode-alist '("/\.zsh[^/]*$" . shell-script-mode))
-(add-to-list 'auto-mode-alist '("/dot-zsh[^/]*$" . shell-script-mode))
-
-;; MESA STUFF https://github.com/jschwab/mesa-major-mode
 (add-to-list 'load-path "~/.emacs.d/emacs_tools/mesa-major-mode/")
 (require 'mesa-mode)
 (require 'run-star-extras)
